adec7183ce3103939e935d737c12a64d29701b31
Jason <jasondoroughonfb@live.com>
2025-06-18 23:14:21 -0700

    Second (Navbar) commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
