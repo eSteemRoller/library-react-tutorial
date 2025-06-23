33afc8d95f9ec1a14e80701c7de7aeb143f3eb43
Jason <jasondoroughonfb@live.com>
2025-06-22 21:23:48 -0700

    Sixth (Highlights Part 3) commit

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,7 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBolt, faBookOpen, faTags } from '@fortawesome/free-solid-svg-icons';
+import Highlight from './ui/Highlights';
 
 export default function Highlights() {
   return (
@@ -11,33 +12,21 @@ export default function Highlights() {
                     Why choose <span className="purple">Library</span>
                 </h2>
                 <div className="highlight__wrapper">
-                    <div className="highlight">
-                        <div className="highlight__img">
-                            <FontAwesomeIcon icon={faBolt} />
-                        </div>
-                        <h3 className="highlight__subtitle">Easy and Quick</h3>
-                        <p className="highlight__para">
-                            Instant online access.
-                        </p>
-                    </div>
-                    <div className="highlight">
-                        <div className="highlight__img">
-                            <FontAwesomeIcon icon={faBookOpen} />
-                        </div>
-                        <h3 className="highlight__subtitle">10,000+ Books</h3>
-                        <p className="highlight__para">
-                            All your favorite categories.
-                        </p>
-                    </div>
-                    <div className="highlight">
-                        <div className="highlight__img">
-                            <FontAwesomeIcon icon={faTags} />
-                        </div>
-                        <h3 className="highlight__subtitle">Affordable</h3>
-                        <p className="highlight__para">
-                            Popular books for only $10.
-                        </p>
-                    </div>
+                    <Highlight 
+                        icon={<FontAwesomeIcon icon={faBolt} />}
+                        title='Easy and Quick'
+                        para='Instant online access.'
+                    />
+                    <Highlight 
+                        icon={<FontAwesomeIcon icon={faBookOpen} />}
+                        title='10,000+ Books'
+                        para='All your favorite categories.'
+                    />
+                    <Highlight 
+                        icon={<FontAwesomeIcon icon={faTags} />}
+                        title='Affordable'
+                        para='Popular books for only $10.'
+                    />
                 </div>
             </div>
         </div>
