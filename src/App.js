4544b0c7a1ba3ad94730e94ce5598ee236a877b9
Jason <jasondoroughonfb@live.com>
2025-06-29 22:28:03 -0700

    Thirteenth (Cart Part 1) commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,37 @@ import Home from './pages/Home';
 import Footer from './components/Footer';
 import Books from './pages/Books';
 import { books } from './data';
+import book from './components/ui/Book';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BookInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
+import { useState } from 'react';
 
 
 export default function App() {
+
+  const [cart, setCart] = useState([]);
+
+  function addToCart() {
+    const dupeItem = cart.find(item => +item.id === +book.id)
+    if (dupeItem) {
+      setCart(cart.map(item => {
+        if (item.id === dupeItem.id) {
+          return {
+            ...item,
+            quantity: item.quantity +1,
+          }
+        }
+        else {
+          return item;
+        }
+      })
+    }
+    else {
+      setCart([...cart, {...book, quantity: 1}])
+    }
+  }
+
   return (
     <Router>
       <div className="App">
@@ -16,7 +42,8 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/books" element={<Books books={books} />} />
-            <Route path="/books/1" element={<BookInfo books={books} />} />
+            <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} />} />
+            <Route path="/cart" element={<Cart books={books} />} />
           </Routes>
           <Footer />
       </div>
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,12 +1,15 @@
 
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import Rating from '../components/ui/Rating'
 import Price from '../components/ui/Price'
+import Book from '../components/ui/Book'
 
 
-export default function BookInfo({ book }) {
+export default function BookInfo({ books, addToCart }) {
+    const { id } = useParams();
+    const book = books.find(book => +book.id === +id);
   return (
     <div id="books__body">
         <main id="books__main">
@@ -22,18 +25,47 @@ export default function BookInfo({ book }) {
                     </div>
                     <div className="book__selected">
                         <figure className="book__selected--figure">
-                            <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--img" />
+                            <img src={book?.url} />
                         </figure>
                         <div className="book__selected--description">
                             <h2 className="book__selected--title">
-                                Crack the Coding Interview
+                                {book?.title}
                             </h2>
-                            <Rating rating={book.rating} />
-                            <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+                            <Rating rating={book?.rating} />
                             <div className="book__selected--price">
-
+                                <Price originalPrice={book?.originalPrice} salePrice={book?.salePrice} />
                             </div>
+                            <div className="book__summary">
+                                <h3 className="book__summary--title">
+                                    Book Summary:
+                                </h3>
+                                <p className="book__summary--para">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, quis aspernatur ipsa quisquam veniam debitis doloribus autem saepe deleniti omnis dolorum neque quasi id consequatur sed adipisci! Commodi, iure ea!
+                                </p>
+                                <p className="book__summary--para">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, quis aspernatur ipsa quisquam veniam debitis doloribus autem saepe deleniti omnis dolorum neque quasi id consequatur sed adipisci! Commodi, iure ea!
+                                </p>
+                            </div>
+                            <button className="btn" onClick={addToCart(book)}>
+                                Add to Cart
+                            </button>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="books__container">
+                <div className="row">
+                    <div className="book__selected--top">
+                        <h2 className="book__selected--title--top">
+                            Recommended Books
+                        </h2>
+                    </div>
+                    <div className="books">
+                        {books
+                            .filter(book => book.rating === 5 && +book.id !== +id)
+                            .slice(0, 4)
+                            .map(book => <Book book={book} key={book.id} />)
+                        }
                     </div>
                 </div>
             </div>
