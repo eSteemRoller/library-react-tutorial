3445a211d1b590a55a4463a89473cbecfb9023da
Jason <jasondoroughonfb@live.com>
2026-08-09 23:42:51 -0700

    chore: update book inventory

--- src/data.js
+++ src/data.js
@@ -66,14 +66,6 @@ export const books = [
     rating: 4.5,
   },
   {
-    id: 8,
-    title: "Cashflow Quadrant",
-    url: "https://m.media-amazon.com/images/I/71+SWQ6xj1L._AC_UF1000,1000_QL80_.jpg",
-    originalPrice: 38.95,
-    salePrice: 17.99,
-    rating: 4.5,
-  },
-  {
     id: 9,
     title: "48 Laws of Power Summary",
     url: "https://m.media-amazon.com/images/I/611X8GI7hpL._AC_UF1000,1000_QL80_.jpg",
