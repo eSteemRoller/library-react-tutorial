6e76e242e154b92c212e7b548072a386a93d3ec5
Jason <jasondoroughonfb@live.com>
2026-08-12 19:17:29 -0700

    chore: format Footer component and update copyright year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,36 @@
-
-import React from 'react'
-import Logo from '../assets/Library.svg'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Logo from "../assets/Library.svg";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
-        <div className="container">
-            <div className="row row__column">
-                <Link to="/">
-                    <figure className='footer__logo'>
-                        <img src={Logo} className="footer__logo--img" alt="" />
-                    </figure>
-                </Link>
-                <div className="footer__list">
-                    <Link to="/" className='footer__link'>Home</Link>
-                    <span className='footer__link no-cursor'>About</span>
-                    <Link to="/books" className='footer__link'>All Books</Link>
-                    <Link to="/cart" className='footer__link'>Your Cart</Link>
-                </div>
-                <div className="footer__copyright">
-                    Copyright &copy; 2025 Library
-                </div>
-            </div>
+      <div className="container">
+        <div className="row row__column">
+          <Link to="/">
+            <figure className="footer__logo">
+              <img src={Logo} className="footer__logo--img" alt="" />
+            </figure>
+          </Link>
+          <div className="footer__list">
+            <Link to="/" className="footer__link">
+              Home
+            </Link>
+            <span className="footer__link no-cursor">About</span>
+            <Link to="/books" className="footer__link">
+              All Books
+            </Link>
+            <Link to="/cart" className="footer__link">
+              Your Cart
+            </Link>
+          </div>
+          <div className="footer__copyright">
+            &copy;&nbsp;{currentYear} Library Demo
+          </div>
         </div>
+      </div>
     </footer>
-  )
+  );
 }
